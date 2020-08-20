1e061f51096c19b6e719232a7e83d14b5a476473
DmitriyIvanovIT <strela199720@gmail.com>
2020-08-20 22:45:30 +0300

    third day

--- script/main.js
+++ script/main.js
@@ -1,1 +1,13 @@
 new WOW().init();
+
+const mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.project-pagination',
+        bulletClass: 'projects-bullet',
+        bulletActiveClass: 'projects-bullet-active',
+        clickable: true
+    },
+    autoplay: {
+        delay: 5000,
+    }
+});
